66593c1b74999643da872585c5927b806c9ddecc
Sergio Trujillo <sergio.trujillo@mxmart.mx>
2024-03-11 10:18:43 -0600

    Validation Only Positive Numbers

--- Day-one-years-to-days/convert-years-to-days.py
+++ Day-one-years-to-days/convert-years-to-days.py
@@ -1,4 +1,10 @@
-print("Write the number of years to convert")
-years=input()
-days = int(years) * 365
-print(f"The number of days is {days}")
+def convert():
+    print("Write the number of years to convert")
+    years=input()
+    if (int(years)<0):
+        print("Only positive numbers")
+        convert()
+    else:
+        days = int(years) * 365
+        print(f"The number of days is {days}")
+convert()
